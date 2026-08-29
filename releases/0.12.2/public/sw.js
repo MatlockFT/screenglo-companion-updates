0753443b25370e1806c2f24f0378981e641e8957
MatlockFT <MatlockFT@users.noreply.github.com>
2026-08-29 01:18:50 -0500

    Release SCREENGLO Companion 0.12.2

---
+++ releases/0.12.2/public/sw.js
@@ -0,0 +1,4 @@
+const CACHE='screenglo-phone-0.12.2-what-to-watch';
+self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(['./','styles.css','app.js','logo.png','icon-192.png','icon-512.png']))));
+self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key))))));
+self.addEventListener('fetch',event=>{if(event.request.method==='GET'&&!new URL(event.request.url).pathname.includes('/api/'))event.respondWith(fetch(event.request).then(response=>{const copy=response.clone();caches.open(CACHE).then(cache=>cache.put(event.request,copy));return response}).catch(()=>caches.match(event.request))) });
